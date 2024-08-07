985dce4c8f30e3104177b70c0c2e66f2d4294f71
EC2 Default User <ec2-user@ip-172-31-17-69.ap-northeast-1.compute.internal>
2024-08-07 18:31:13 +0000

    [Add] error message

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,15 +3,23 @@ class BooksController < ApplicationController
   # end
 
   def index
-    @book = Book.new
+    @book_new = Book.new
     @books = Book.all
+    @user = current_user
+    @book = Book.new
   end
   
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @book_new = Book.new
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
   end
   
   def destroy
@@ -22,6 +30,21 @@ class BooksController < ApplicationController
   
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
+    @user = User.find(@book.user_id)
+  end
+  
+  def edit
+    @book =  Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
   end
   
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
+    @book_new = Book.new
     @user = User.find(params[:id])
     @books = @user.books
   end
@@ -10,11 +11,17 @@ class UsersController < ApplicationController
   
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
+
   
   def index
+    @book_new = Book.new
+    @user = User.find(current_user.id)
     @users = User.all
   end
   
