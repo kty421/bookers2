57e5df4710a23e0d8ced21a2ccb9d229f218f675
EC2 Default User <ec2-user@ip-172-31-17-69.ap-northeast-1.compute.internal>
2024-08-08 10:44:16 +0000

    specTest clear

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,5 @@
 class BooksController < ApplicationController
-  # def new
-  # end
+  before_action :is_matching_login_user, only: [:edit, :update]
 
   def index
     @book_new = Book.new
@@ -13,6 +12,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
+      flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
     else
       @book_new = Book.new
@@ -41,6 +41,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
       redirect_to book_path(@book)
     else
       render :edit
@@ -51,5 +52,14 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+  
+  
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    user = book.user
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
   def show
     @book_new = Book.new
     @user = User.find(params[:id])
@@ -8,27 +9,36 @@ class UsersController < ApplicationController
   def edit
     @user =  User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
       redirect_to user_path(@user.id)
     else
       render :edit
     end
   end
 
-  
+
   def index
     @book_new = Book.new
     @user = User.find(current_user.id)
     @users = User.all
   end
-  
+
    private
 
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
-  
+
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
+  end
+
 end
